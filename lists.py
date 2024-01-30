43a5ce070a1c68c9d3cb008eaa93d0b786678862
SumitSohal07 <145088567+SumitSohal07@users.noreply.github.com>
2024-01-30 17:10:02 -0500

    Add files via upload
    Today I revised the list usage in pyhton to create further programs.

---
+++ lists.py
@@ -0,0 +1,40 @@
+# fruits = ["apple", "banana", "mango"]
+# i = 0
+
+# while i < len(fruits):
+#     print(fruits[i])
+#     i =+1
+
+# fruits = ["apple", "banana", "cherry"]
+# i = 0
+
+# while i < len(fruits):
+#     print(fruits[i])
+#     i += 1
+
+numbers = [1,2,3,4,5]
+alpha = ['h','e','l','l','o']
+newlist=[]
+
+
+print(len(alpha))
+print(sum(numbers))
+# # list comprehensions
+# for x in numbers:
+#     if x%2==0:
+#         print(x)
+#         newlist.append(x)
+# print(newlist)
+        
+# # iterate the list items with for loop
+
+# for x in alpha:
+#     print(x)
+
+
+# modify the list
+# alpha.append('fruit')
+# numbers.pop(4)
+
+# print(numbers)
+# print(alpha)
